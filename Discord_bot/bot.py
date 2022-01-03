a2a890d0ac31228c895ab55a5667e4c8ab26c95b
YeAlice <a0919191954@gmail.com>
2022-01-03 13:17:09 +0800

    Bot_json

--- Discord_bot/bot.py
+++ Discord_bot/bot.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+import json
+
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
 # discord.py 1.5 版本更新 訂閱所有事情
 intents = discord.Intents.all()
 
@@ -9,19 +14,20 @@ Bot = commands.Bot(command_prefix = 'Bot ', intents = intents) # 呼叫機器人
 async def on_ready(): # 判斷機器人是否連線
     print(">>Bot is Online<<")
 
+
 @Bot.event # 事件
 async def on_member_join(member): # 判斷是否有人員加入
-    channel = Bot.get_channel(927410875168604161) # bot_message 伺服器
+    channel = Bot.get_channel(int[jdata['Bot_message']]) # bot_message 伺服器
     await channel.send(f'{member} 加入了伺服器！') # send 一定要加 await
     print(f'{member} join!')
 
 @Bot.event # 事件
 async def on_member_remove(member): # 判斷是否有人員離開
-    channel = Bot.get_channel(927410875168604161) # bot_message 伺服器
+    channel = Bot.get_channel(int[jdata['Bot_message']]) # bot_message 伺服器
     await channel.send(f'{member} 離開了伺服器！') # send 一定要加 await
     print(f'{member} leave!')
 @Bot.command()
 async def ping(ctx): # 網路延遲 ctx = context (上下文)
     await ctx.send(f'{round(Bot.latency * 1000)} (毫秒)') # latency 延遲
 
-Bot.run('') # 啟動
+Bot.run(jdata['TOKEN']) # 啟動
