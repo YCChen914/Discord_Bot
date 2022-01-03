ba6f13e544f3fd73e4af6fb7cda540a5d1af0cd5
YeAlice <a0919191954@gmail.com>
2022-01-03 11:43:49 +0800

    Bot_online

--- Discord_bot/bot.py
+++ Discord_bot/bot.py
@@ -7,4 +7,4 @@ Bot = commands.Bot(command_prefix = 'Bot') # 呼叫機器人 Bot 的命令字首
 async def on_ready():
     print(">>Bot is Online<<")
 
-Bot.run('OTI3MzgwNjU5ODMwNDA3MTk4.YdJYiw.iXAr4xS-9_oPCRvYDtAVIWVY-hk') # 啟動
+Bot.run('') # 啟動
