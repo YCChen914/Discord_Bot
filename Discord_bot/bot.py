ff91121a61ace2c28ab0f0b4819ef1feb59d7d71
YeAlice <a0919191954@gmail.com>
2022-01-03 12:32:15 +0800

    bot_join/leave

--- Discord_bot/bot.py
+++ Discord_bot/bot.py
@@ -1,10 +1,24 @@
 import discord
 from discord.ext import commands
+# discord.py 1.5 版本更新 訂閱所有事情
+intents = discord.Intents.all()
 
-Bot = commands.Bot(command_prefix = 'Bot') # 呼叫機器人 Bot 的命令字首
+Bot = commands.Bot(command_prefix = 'Bot', intents = intents) # 呼叫機器人 Bot 的命令字首
 
-@Bot.event
-async def on_ready():
+@Bot.event # 事件
+async def on_ready(): # 判斷機器人是否連線
     print(">>Bot is Online<<")
+
+@Bot.event # 事件
+async def on_member_join(member): # 判斷是否有人員加入
+    channel = Bot.get_channel(927410875168604161) # bot_message
+    await channel.send(f'{member} 加入了伺服器！') # send 一定要加 await
+    print(f'{member} join!')
+
+@Bot.event # 事件
+async def on_member_remove(member): # 判斷是否有人員離開
+    channel = Bot.get_channel(927410875168604161) # bot_message
+    await channel.send(f'{member} 離開了伺服器！') # send 一定要加 await
+    print(f'{member} leave!')
 
 Bot.run('') # 啟動
