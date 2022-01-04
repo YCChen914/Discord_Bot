0704d730ebe8404a1e646254169a673e9a9930c1
YeAlice <a0919191954@gmail.com>
2022-01-04 20:01:47 +0800

    Bot_Embed

--- Discord_bot/cmds/main.py
+++ Discord_bot/cmds/main.py
@@ -8,6 +8,7 @@ sys.path.append(rootPath)
 import discord
 from discord.ext import commands
 from core.classes import Cog_Extension
+import datetime
 
 class Main(Cog_Extension): # 物件導向 繼承類別
 
@@ -18,6 +19,16 @@ class Main(Cog_Extension): # 物件導向 繼承類別
     @commands.command()
     async def hi(self, ctx): # 打招呼回覆
         await ctx.send('你好呀~~~')
-        
+    
+    # 注意 value 一定要有值
+    @commands.command()
+    async def em(self, ctx): # 嵌入訊息 https://cog-creators.github.io/discord-embed-sandbox/
+        embed=discord.Embed(title="自由開源軟體", url="https://moodle.thu.edu.tw/", description="一堂非常有意義的課程", color=0x0597d6, timestamp = datetime.datetime.now()) # 不是台灣的時間
+        embed.set_author(name="葉子", url="https://github.com/")
+        embed.set_thumbnail(url="https://pcstrike.com/wp-content/uploads/2021/09/Best-Discord-Bots.png")
+        embed.add_field(name="Discord Bot", value="FY_Bot", inline=True)
+        embed.set_footer(text="謝謝觀看")
+        await ctx.send(embed=embed)
+
 def setup(Bot):
     Bot.add_cog(Main(Bot))
